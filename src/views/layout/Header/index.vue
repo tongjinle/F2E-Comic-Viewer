db8e2d1d431059197321750e447c66139cc08bd8
tongjinle <1328192478@qq.com>
2019-05-12 03:19:01 +0800

    title

--- src/views/layout/Header/index.vue
+++ src/views/layout/Header/index.vue
@@ -1,53 +1,52 @@
 <template>
-  <head id='header'>
-    <router-link :to="{name:'home'}" tag="div">
-      <div class="title">Comicomic</div>
-    </router-link>
-    <div class='github'>
-      <a href='https://github.com/runkids/F2E-Comic-Viewer' target="_blank"><i class="fab fa-github"></i></a>
-    </div>
-  </head>
+<head id="header">
+  <router-link :to="{name:'home'}" tag="div">
+    <div class="title">Tea Comic</div>
+  </router-link>
+  <div class="github">
+    <!-- <a href='https://github.com/runkids/F2E-Comic-Viewer' target="_blank"><i class="fab fa-github"></i></a> -->
+  </div>
+</head>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 
 <style lang="scss">
-  @import '../../../assets/style/main.scss';
+@import "../../../assets/style/main.scss";
 
-  #header{
-    background-color: $black_color;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 74px;
-    position: relative;
-    .title{
-      cursor: pointer;
-      font-style: italic;
-      font-weight: 900;
-      font-size: 36px;
-      color: $light_green;
-    }
-    .github{
-      position: absolute;
-      right: 20px;
-      a{
+#header {
+  background-color: $black_color;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 74px;
+  position: relative;
+  .title {
+    cursor: pointer;
+    font-style: italic;
+    font-weight: 900;
+    font-size: 36px;
+    color: $light_green;
+  }
+  .github {
+    position: absolute;
+    right: 20px;
+    a {
+      color: #fff;
+      &:visited {
         color: #fff;
-        &:visited{
-          color: #fff;
-        }
       }
-      i{
-        font-size: 35px;
-        cursor: pointer;
-        &:hover{
-          animation: .8s github alternate infinite;
-        }
+    }
+    i {
+      font-size: 35px;
+      cursor: pointer;
+      &:hover {
+        animation: 0.8s github alternate infinite;
       }
     }
   }
+}
 </style>
